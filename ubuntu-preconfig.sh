471c30ff0f5c3ecf946a8b93795acadeefce949c
mordilloSan <121123040+mordilloSan@users.noreply.github.com>
2023-11-12 18:04:51 +0000

    up

--- ubuntu-preconfig.sh
+++ ubuntu-preconfig.sh
@@ -329,113 +329,6 @@ change_renderer() {
 ###################
 # Cockpit Section #
 ###################
-function get_base_distro() {
-        local distro=$(cat /etc/os-release | grep '^ID_LIKE=' | head -1 | sed 's/ID_LIKE=//' | sed 's/"//g' | awk '{print $1}')
-
-        if [ -z "$distro" ]; then
-                distro=$(cat /etc/os-release | grep '^ID=' | head -1 | sed 's/ID=//' | sed 's/"//g' | awk '{print $1}')
-        fi
-
-        echo $distro
-}
-function get_distro() {
-        local distro=$(cat /etc/os-release | grep '^ID=' | head -1 | sed 's/ID=//' | sed 's/"//g' | awk '{print $1}')
-
-        echo $distro
-}
-function get_version_id() {
-        local version_id=$(cat /etc/os-release | grep '^VERSION_ID=' | head -1 | sed 's/VERSION_ID=//' | sed 's/"//g' | awk '{print $1}' | awk 'BEGIN {FS="."} {print $1}')
-
-        echo $version_id
-}
-add_45repo(){
-    GreyStart
-    euid=$(id -u)
-
-    if [ $euid -ne 0 ]; then
-            echo -e '\nYou must be root to run this utility.\n'
-    fi
-
-    distro=$(get_base_distro)
-    custom_distro=$(get_distro)
-    distro_version=$(get_version_id)
-
-    Show 2 "Detected Debian-based distribution. Continuing..."
-
-    items=$(find /etc/apt/sources.list.d -name 45drives.list)
-
-    if [[ -z "$items" ]]; then
-        Show 2 "There were no existing 45Drives repos found. Setting up the new repo..."
-    else
-        count=$(echo "$items" | wc -l)
-        Show 2 "There were $count 45Drives repo(s) found. Archiving..."
-        GreyStart
-        mkdir -p /opt/45drives/archives/repos
-
-        mv /etc/apt/sources.list.d/45drives.list /opt/45drives/archives/repos/45drives-$(date +%Y-%m-%d).list
-
-        Show 2 "The obsolete repos have been archived to '/opt/45drives/archives/repos'. Setting up the new repo..."
-    fi
-
-    if [[ -f "/etc/apt/sources.list.d/45drives.sources" ]]; then
-        rm -f /etc/apt/sources.list.d/45drives.sources
-    fi
-
-    Show 2 "Updating ca-certificates to ensure certificate validity..."
-    GreyStart
-    apt-get update
-    apt-get install ca-certificates -y
-
-    wget -qO - https://repo.45drives.com/key/gpg.asc | gpg --pinentry-mode loopback --batch --yes --dearmor -o /usr/share/keyrings/45drives-archive-keyring.gpg
-
-    res=$?
-
-    if [ "$res" -ne "0" ]; then
-        Show 1 "Failed to add the gpg key to the apt keyring. Please review the above error and try again."
-        exit 1
-    fi
-
-
-    wget -qO - https://repo.45drives.com/lists/45drives.sources -o /etc/apt/sources.list.d/45drives.sources
-
-    res=$?
-
-    if [ "$res" -ne "0" ]; then
-        Show 1 "Failed to download the new repo file. Please review the above error and try again."
-        exit 1
-    fi
-
-    lsb_release_cs=$(lsb_release -cs)
-
-    if [[ "$lsb_release_cs" == "" ]]; then
-        Show 1 "Failed to fetch the distribution codename. This is likely because the command, 'lsb_release' is not available. Please install the proper package and try again. (apt install -y lsb-core)"
-        exit 1
-    fi
-
-    sed -i "s/focal/$lsb_release_cs/g" /etc/apt/sources.list.d/45drives.sources
-
-    res=$?
-
-    if [ "$res" -ne "0" ]; then
-            Show 1 "Failed to update the new repo file. Please review the above error and try again."
-            exit 1
-    fi
-    echo ""
-    Show 0 "The new repo file has been downloaded. Updating your package lists..."
-    GreyStart
-
-    apt-get update -y
-
-    res=$?
-
-    if [ "$res" -ne "0" ]; then
-            Show 1 "Failed to run '$pm_bin update -y'. Please review the above error and try again."
-            exit 1
-    fi
-
-    Show 0 "Success! Your repo has been updated to our new server!"
-
-}
 install_cockpit() {
 
     # Install cockpit and cockpit related things
@@ -447,7 +340,6 @@ install_cockpit() {
     echo ""   
     GreyStart
     # Add the 45 drives repo
-    #add_45repo
     #curl -sSL https://repo.45drives.com/setup | bash
     wget -qO - https://repo.45drives.com/key/gpg.asc | apt-key add -
     curl -o /etc/apt/sources.list.d/45drives.sources https://repo.45drives.com/lists/45drives.sources 
