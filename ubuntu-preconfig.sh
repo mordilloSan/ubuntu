ec80ebd8f19430f6d2948173e3bee8d81b1c64d5
mordilloSan <121123040+mordilloSan@users.noreply.github.com>
2023-11-12 19:14:58 +0000

    up

--- ubuntu-preconfig.sh
+++ ubuntu-preconfig.sh
@@ -344,7 +344,7 @@ install_cockpit() {
     # Add the 45 drives repo
     #curl -sSL https://repo.45drives.com/setup | bash
     wget -qO - https://repo.45drives.com/key/gpg.asc | apt-key add -
-    curl -os -w /etc/apt/sources.list.d/45drives.sources https://repo.45drives.com/lists/45drives.sources 
+    curl -o -s -w /etc/apt/sources.list.d/45drives.sources https://repo.45drives.com/lists/45drives.sources 
     res=$?
     if [[ $res != 0 ]]; then
 		echo ""
