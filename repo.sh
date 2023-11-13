1d2d61ea0f3ead74eba03224caa49fd39e9a1132
mordilloSan <121123040+mordilloSan@users.noreply.github.com>
2023-11-13 16:47:29 +0000

    fixing repo

--- repo.sh
+++ repo.sh
@@ -1,37 +1,5 @@
 #!/bin/bash
 
-#function get_base_distro() {
-#	local distro=$(cat /etc/os-release | grep '^ID_LIKE=' | head -1 | sed 's/ID_LIKE=//' | sed 's/"//g' | awk '{print $1}')
-#
-#	if [ -z "$distro" ]; then
-#		distro=$(cat /etc/os-release | grep '^ID=' | head -1 | sed 's/ID=//' | sed 's/"//g' | awk '{print $1}')
-#	fi
-#
-#	echo $distro
-#}
-
-#function get_distro() {
-#	local distro=$(cat /etc/os-release | grep '^ID=' | head -1 | sed 's/ID=//' | sed 's/"//g' | awk '{print $1}')
-#	
-#	echo $distro
-#}
-
-#function get_version_id() {
-#	local version_id=$(cat /etc/os-release | grep '^VERSION_ID=' | head -1 | sed 's/VERSION_ID=//' | sed 's/"//g' | awk '{print $1}' | awk 'BEGIN {FS="."} {print $1}')
-#	
-#	echo $version_id
-#}
-
-#euid=$(id -u)
-#
-#if [ $euid -ne 0 ]; then
-#	echo -e '\nYou must be root to run this utility.\n'
-#fi
-
-#distro=$(get_base_distro)
-#custom_distro=$(get_distro)
-#distro_version=$(get_version_id)
-
 	items=$(find /etc/apt/sources.list.d -name 45drives.sources)
 	if [[ -z "$items" ]]; then
 		echo "There were no existing 45Drives repos found. Setting up the new repo..."
@@ -46,8 +14,7 @@
 		fi
 	fi
 	echo "Updating ca-certificates to ensure certificate validity..."
-	apt update
-	apt install ca-certificates -y
+	apt-get install ca-certificates -y
 	wget -qO - https://repo.45drives.com/key/gpg.asc | gpg --pinentry-mode loopback --batch --yes --dearmor -o /usr/share/keyrings/45drives-archive-keyring.gpg
 	res=$?
 	if [ "$res" -ne "0" ]; then
@@ -65,33 +32,28 @@
 		echo "Failed to fetch the distribution codename. This is likely because the command, 'lsb_release' is not available. Please install the proper package and try again. (apt install -y lsb-core)"
 		exit 1
 	fi
-	if [[ "$lsb_release_cs" != "focal" ]] && [[ "$lsb_release_cs" != "bionic" ]]; then
-	read -p "You are on an unsupported version of Debian. Would you like to use 'focal' packages? [y/N] " response
-
-		case $response in
-			[yY]|[yY][eE][sS])
-				echo
-				;;
-			*)
-				echo "Exiting..."
-				exit 1
-				;;
-		esac
-
-		lsb_release_cs="focal"
-	fi
-
+#	if [[ "$lsb_release_cs" != "focal" ]] && [[ "$lsb_release_cs" != "bionic" ]]; then
+#	read -p "You are on an unsupported version of Debian. Would you like to use 'focal' packages? [y/N] " response
+#
+#		case $response in
+#			[yY]|[yY][eE][sS])
+#				echo
+#				;;
+#			*)
+#				echo "Exiting..."
+#				exit 1
+#				;;
+#		esac
+#
+#		lsb_release_cs="focal"
+#	fi
 	sed -i "s/focal/$lsb_release_cs/g" /etc/apt/sources.list.d/45drives.sources
-
 	res=$?
-
 	if [ "$res" -ne "0" ]; then
 		echo "Failed to update the new repo file. Please review the above error and try again."
 		exit 1
 	fi
-
 	echo "The new repo file has been downloaded."
-
 	echo "Success! Your repo has been updated to our new server!"
 
 echo -e "\nThis command has been run on a distribution that is not supported by the 45Drives Team.\n\nIf you believe this is a mistake, please contact our team at repo@45drives.com!\n"
