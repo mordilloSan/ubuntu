3e38dafb871b2c114fdaa36e1c419ddf2c3dc1d0
mordilloSan <121123040+mordilloSan@users.noreply.github.com>
2023-11-13 16:29:09 +0000

    up

--- repo.sh
+++ repo.sh
@@ -60,32 +60,23 @@ if [ "$distro" == "debian" ]; then
 	echo "Updating ca-certificates to ensure certificate validity..."
 	apt update
 	apt install ca-certificates -y
-
 	wget -qO - https://repo.45drives.com/key/gpg.asc | gpg --pinentry-mode loopback --batch --yes --dearmor -o /usr/share/keyrings/45drives-archive-keyring.gpg
-
 	res=$?
-
 	if [ "$res" -ne "0" ]; then
 		echo "Failed to add the gpg key to the apt keyring. Please review the above error and try again."
 		exit 1
 	fi
-
 	curl -sSL https://repo.45drives.com/lists/45drives.sources -o /etc/apt/sources.list.d/45drives.sources
-
 	res=$?
-
 	if [ "$res" -ne "0" ]; then
 		echo "Failed to download the new repo file. Please review the above error and try again."
 		exit 1
 	fi
-
 	lsb_release_cs=$(lsb_release -cs)
-
 	if [[ "$lsb_release_cs" == "" ]]; then
 		echo "Failed to fetch the distribution codename. This is likely because the command, 'lsb_release' is not available. Please install the proper package and try again. (apt install -y lsb-core)"
 		exit 1
 	fi
-
 	if [[ "$lsb_release_cs" != "focal" ]] && [[ "$lsb_release_cs" != "bionic" ]]; then
 	read -p "You are on an unsupported version of Debian. Would you like to use 'focal' packages? [y/N] " response
 
