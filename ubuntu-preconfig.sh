1aad1837cf38fe6743cae6c621d442860ad5fe3f
mordilloSan <121123040+mordilloSan@users.noreply.github.com>
2023-11-14 16:10:29 +0000

    debug snap

--- ubuntu-preconfig.sh
+++ ubuntu-preconfig.sh
@@ -395,9 +395,8 @@ Remove_snap(){
     Show 2 "Removing snap"
     local res
     #Getting List of snaps installed
-    #Retrieve first word of each line
-    SNAP_LIST=$(awk '{print $1}' "$[snap list]" | sed '1d')
-    #SNAP_LIST=$(snap list | sed '1d')
+    SNAP_LIST=$(snap list | sed '1d')
+    SNAP_LIST= tail -n +2 $SNAP_LIST
     Show 2 "$SNAP_LIST"
     if [ $(dpkg-query -W -f='${Status}' "snapd" 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
         Show 0 "snap not installed."
