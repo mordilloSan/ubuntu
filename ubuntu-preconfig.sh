8b056ae19611efcfd8fa7e1f07cfd5861455be89
mordilloSan <121123040+mordilloSan@users.noreply.github.com>
2023-11-14 11:54:35 +0000

    cosmetics

--- ubuntu-preconfig.sh
+++ ubuntu-preconfig.sh
@@ -226,7 +226,8 @@ init_network() {
 add_45repo(){
 	items=$(find /etc/apt/sources.list.d -name 45drives.sources)
 	if [[ -z "$items" ]]; then
-		Show 4 "There were no existing 45Drives repos found. Setting up the new repo..."
+        echo -e "${aCOLOUR[2]}There were no existing 45Drives repos found. Setting up the new repo..."
+#		Show 4 "There were no existing 45Drives repos found. Setting up the new repo..."
 	else
 		count=$(echo "$items" | wc -l)
 		Show 4 "There were $count 45Drives repo(s) found. Archiving..."
