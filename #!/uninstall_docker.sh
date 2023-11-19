a61b6b3372e06fc31510e061808159de93b77d84
mordilloSan <121123040+mordilloSan@users.noreply.github.com>
2023-11-19 23:16:17 +0000

    Delete #! directory

--- #!/uninstall_docker.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-sudo apt-get purge docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras -y
-sudo rm -rf /var/lib/docker
-sudo rm -rf /var/lib/containerd
