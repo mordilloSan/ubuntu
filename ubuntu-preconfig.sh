9587a9b15ba7bd4383d37f100d69373e4a94bef0
mordilloSan <121123040+mordilloSan@users.noreply.github.com>
2023-11-20 15:49:06 +0000

    up

--- ubuntu-preconfig.sh
+++ ubuntu-preconfig.sh
@@ -30,7 +30,7 @@ Start (){
     readonly GREEN_SEPARATOR="${aCOLOUR[0]}:$COLOUR_RESET"
     #Working Directory in user home folder
     echo "The login user is ${SUDO_USER:-$(whoami)}"
-    WORK_DIR= echo "${SUDO_USER:-$(whoami)}"
+    WORK_DIR= ${SUDO_USER:-$(whoami)}
     echo "$WORK_DIR"
     WORK_DIR=$(echo "/home/${WORK_DIR}")
     echo "$WORK_DIR"
