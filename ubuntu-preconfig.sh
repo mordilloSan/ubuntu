78fbddfd81df27e426247a8d8e7dfa61735f3fc9
mordilloSan <121123040+mordilloSan@users.noreply.github.com>
2023-11-21 21:10:58 +0000

    bug

--- ubuntu-preconfig.sh
+++ ubuntu-preconfig.sh
@@ -404,7 +404,9 @@ change_renderer() {
     systemctl stop systemd-networkd.service
     systemctl stop systemd-networkd-wait-online.service
     ln -sf /run/systemd/resolve/resolv.conf /etc/resolv.conf
-    mv /usr/lib/NetworkManager/conf.d/10-globally-managed-devices.conf  /usr/lib/NetworkManager/conf.d/10-globally-managed-devices.conf.backup
+    if [ -f /usr/lib/NetworkManager/conf.d/10-globally-managed-devices.conf ]; then
+        mv /usr/lib/NetworkManager/conf.d/10-globally-managed-devices.conf  /usr/lib/NetworkManager/conf.d/10-globally-managed-devices.conf.backup
+    fi
     sed -i '/^managed/s/false/true/' /etc/NetworkManager/NetworkManager.conf
     systemctl restart NetworkManager 
 }
