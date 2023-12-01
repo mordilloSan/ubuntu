bdb57f5f4c8466b22f3a6332d8b6a7b1bd836bb9
mordilloSan <121123040+mordilloSan@users.noreply.github.com>
2023-12-01 13:02:41 +0000

    up

--- ubuntu-preconfig.sh
+++ ubuntu-preconfig.sh
@@ -17,7 +17,7 @@ Start (){
     if [[ ! -d "$WORK_DIR" ]]; then
         mkdir "$WORK_DIR"
     fi
-    readonly PACKAGES=("lm-sensors" "htop" "network-manager" "net-tools" "cockpit" "cockpit-navigator" "realmd" "tuned" "udisks2-lvm2" "samba" "winbind" "nfs-kernel-server" "nfs-common" "cockpit-file-sharing" "cockpit-pcp")
+    readonly PACKAGES=("lm-sensors" "htop" "network-manager" "net-tools" "cockpit" "cockpit-navigator" "realmd" "tuned" "udisks2-lvm2" "samba" "winbind" "nfs-kernel-server" "nfs-common" "cockpit-file-sharing" "cockpit-pcp" "wireguard-tools")
     readonly SERVICES=("cockpit.socket" "NetworkManager" "NetworkManager-wait-online.service")
     readonly NETWORK_SERVICES=("systemd-networkd.socket" "systemd-networkd.service" "systemd-networkd-wait-online.service")
     readonly NAS_IP="192.168.1.65"
