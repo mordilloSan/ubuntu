42361d46374737b6845c72c3865e96b0aa95e896
mordilloSan <121123040+mordilloSan@users.noreply.github.com>
2023-12-03 17:44:12 +0000

    up

--- ubuntu-preconfig.sh
+++ ubuntu-preconfig.sh
@@ -417,7 +417,7 @@ NFS_Mount(){
             Check_Success "NAS NFS mount"
             Show 2 "Making the mount permanent"
             if ! grep "$WORK_DIR"/docker /etc/fstab; then
-                echo "$NAS_IP:/volume2/docker $WORK_DIR/docker  nfs      defaults    0       0" >> /etc/fstab
+                echo "$NAS_IP:/volume2/server/docker $WORK_DIR/docker  nfs      defaults    0       0" >> /etc/fstab
                 Check_Success "NFS mount on boot"
             else
                 Show 0 "NFS Mount on boot"
