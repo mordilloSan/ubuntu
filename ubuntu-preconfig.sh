246fa93a01f2820d80ac5751b3afaed067fb015e
mordillo <121123040+mordilloSan@users.noreply.github.com>
2024-02-10 21:52:49 +0000

    update

--- ubuntu-preconfig.sh
+++ ubuntu-preconfig.sh
@@ -439,7 +439,7 @@ Containers(){
     fi
     # Start Portainer
     echo "Starting Portainer"
-    docker compose --project-directory ~/docker/portainer/ up -d
+    docker compose -d --project-directory ~/docker/portainer/ up 
 }
 Remove_cloudinit(){
     Show 2 "Removing cloud-init"
