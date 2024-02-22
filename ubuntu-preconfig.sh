8595741a435db4289241591c5e7d4e524ab90d6e
mordillo <121123040+mordilloSan@users.noreply.github.com>
2024-02-22 16:10:00 +0000

    update

--- ubuntu-preconfig.sh
+++ ubuntu-preconfig.sh
@@ -129,11 +129,12 @@ Check_Connection(){
 		Show 1 "No internet connection"
     fi
     Show 0 "Internet : \e[33mOnline\e[0m"
-    nas=$(wget --timeout=3 192.168.1.2 -q -t 1 --spider $NAS_IP ; echo $?)
+    nas=$(wget --timeout=3 -q -t 1 --spider $NAS_IP ; echo $?)
     if [ "$nas" != 0 ]; then
 		Show 3 "No NAS present"
-    fi
+    else
         Show 0 "NAS ($NAS_IP): \e[33mOnline\e[0m"
+    fi
 }
 Check_Success(){
     if [[ $1 != 0 ]]; then
